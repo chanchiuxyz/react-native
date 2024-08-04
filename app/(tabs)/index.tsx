cc637237f6854b786765ae7356090b83fe7de2ae
chanchiuxyz <chanchiuxyz@gmail.com>
2024-08-04 14:48:01 -0700

    touchable

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,7 @@
-import { Image, StyleSheet, Platform, View, Text, TextInput, Dimensions, Button } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, TextInput, Dimensions, Button,
+  TouchableHighlight,
+  TouchableOpacity,
+ } from 'react-native';
 import { useState} from 'react'
 
 import { HelloWave } from '@/components/HelloWave';
@@ -6,12 +9,13 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 // import { View } from 'react-native-reanimated/lib/typescript/Animated';
-
+const Separator = () => <View style={styles.separator} />;
 export default function HomeScreen() {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+  const [mobile, setMobile] = useState('')
   const doLogin = () => {
-    alert(name)
+    // alert(name)
   }
   return (
     <View style={[styles.container]}>
@@ -24,6 +28,8 @@ export default function HomeScreen() {
 
       />
 
+    
+
      <TextInput
         style={[styles.input]}
         placeholder='Enter your password'
@@ -34,10 +40,43 @@ export default function HomeScreen() {
 
       />
 
+    <TextInput
+        style={[styles.input]}
+        placeholder='Enter your mobile'
+        value={mobile}
+        onChangeText={(text) => setMobile(text)}
+        keyboardType='number-pad'
+
+
+
+      /> 
       <View style={[styles.btn]}>
 
-         <Button title='Login' onPress={doLogin}></Button>
+        <Button title='-Login-' onPress={doLogin}></Button>
+   
       </View>
+      <Separator />
+
+    
+          <TouchableHighlight 
+            onPress={() => {alert('touchablehighlight')}}
+          >
+              <View>
+                <Text style={[styles.item]}>TouchableHighlight</Text>
+              </View>
+              
+
+          </TouchableHighlight>
+          <TouchableOpacity
+            onPress={() => {alert('touchableopacity')}}
+          >
+              <View>
+                <Text style={[styles.item]}>TouchableHighlight</Text>
+              </View>
+
+          </TouchableOpacity>
+
+      
 
     </View>
   );
@@ -61,6 +100,19 @@ const styles = StyleSheet.create({
   btn: {
     margin: 10,
 
+  },
+  item: {
+    margin: 10,
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 
 });
